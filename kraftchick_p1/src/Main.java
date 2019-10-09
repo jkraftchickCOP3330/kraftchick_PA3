71c925480069703ac01cb136f5f800c567c4ac1c
jkraftchick <54610324+jkraftchick@users.noreply.github.com>
2019-10-09 13:52:57 -0400

    part 1

--- kraftchick_p1/src/Main.java
+++ kraftchick_p1/src/Main.java
@@ -1,6 +1,37 @@
+import java.security.SecureRandom;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        SecureRandom random = new SecureRandom();
+        Scanner scan = new Scanner(System.in);
+
+        int num1, num2, product, userInput;
+        boolean correct = false;
+
+
+        num1 = random.nextInt(10);
+        num2 = random.nextInt(10);
+        product = num1 * num2;
+        userInput = -1;
+
+        while (!correct)
+        {
+            System.out.print("How much is " + num1 + " times " + num2 + "? ");
+            userInput = scan.nextInt();
+
+            if (userInput == product)
+            {
+                System.out.println("Correct!");
+                correct = true;
+                break;
+            }
+            else
+            {
+                System.out.println("No. Please try again.");
+            }
+        }
     }
 }
